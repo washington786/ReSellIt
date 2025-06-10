c93f71bd426faab0f3887922ac6baadbf36ceb0a
washington786 <dkmawasha@gmail.com>
2025-06-10 09:57:42 +0200

    fixed schema error

--- src/schemas/message.ts
+++ src/schemas/message.ts
@@ -1,5 +1,5 @@
 import * as yup from "yup";
 
 export const notificationSchema = yup.object().shape({
-    message: yup.string().required("Message is required");
+    message: yup.string().required("Message is required")
 })
